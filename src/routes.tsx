a3e2aa91f1f9c9b4fb701a18a2401f0552d8f5b1
Ana Lucia Bolico <51336956+analuciabolico@users.noreply.github.com>
2022-01-20 22:32:34 -0300

    add integration with backend (#14)
    * add integration with backend
    
    * remove initial page
    
    * rollback CREATE page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import { ROUTES as R } from "#/src/constants";
 
-import HomePage from "#/src/pages/Home";
+// import HomePage from "#/src/pages/Home";
 import CreatePage from "@/pages/Create";
 import MakerPage from "@/pages/Maker";
 import NotFoundPage from "@/pages/NotFound";
@@ -11,8 +11,8 @@ const Routes: React.FC = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path={R.HOME()} element={<HomePage />} />
-				<Route path={R.CREATE()} element={<CreatePage />} />
+				{/* <Route path={R.HOME()} element={<HomePage />} /> */}
+				<Route path={R.HOME()} element={<CreatePage />} />
 				<Route path={R.MAKER()} element={<MakerPage />} />
 				<Route path={R.NOT_FOUND()} element={<NotFoundPage />} />
 			</Switch>
