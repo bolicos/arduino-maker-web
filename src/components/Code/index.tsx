b2ce95ab8acee82ca797644132c82b65f9910bf1
analuciabolico <analuciabolico.dev@gmail.com>
2022-01-22 15:15:20 -0300

    add color into code and fix selects

---
+++ src/components/Code/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+export interface Props {
+	code: string;
+	language: string;
+}
+
+const Code: React.FC<Props> = ({ code, language }) => {
+	return (
+		<SyntaxHighlighter language={language} style={docco} showLineNumbers={true} >
+			{code}
+		</SyntaxHighlighter>
+	);
+};
+
+export default Code;
