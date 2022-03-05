77c8407e4d2e42c47a033b5c04d4b96a7a96fea7
analuciabolico <analuciabolico.dev@gmail.com>
2022-03-05 18:31:50 -0300

    feat: create new component QRCodeCanvas

---
+++ src/components/QRCodeCanvas/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef, useState } from "react";
+import QRCode from "qrcode";
+
+
+interface Props {
+  text: string;
+}
+
+const QRCodeCanvas: React.FC<Props> = ({ text }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isError, setError] = useState(false);
+  const errorMessage = "Ocorreu um erro ao gerar o QRCode";
+
+  const toError = (error: Error) => {
+    if (error) {
+      console.log("Canvas QRCode Error");
+      console.log(error.message);
+      setError(true);
+    } else console.log("Canvas QRCode Succes");
+  };
+
+  useEffect(() => {
+    QRCode.toCanvas(canvasRef.current, text, toError);
+  },[text]);
+
+  return (
+    <div>
+      {isError
+        ? <>{errorMessage}</>
+        : <canvas ref={canvasRef} id="canvas" />
+      }
+    </div>
+  );
+};
+
+export default QRCodeCanvas;
