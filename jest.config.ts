d9d6056dd8aa15af9de74ecdd120c615646bb530
analuciabolico <analuciabolico.dev@gmail.com>
2022-09-27 21:42:50 -0300

    try fix test but not working

--- jest.config.ts
+++ jest.config.ts
@@ -1,30 +1,46 @@
-// "jest": {
-//     "setupFilesAfterEnv": [
-//       "<rootDir>/src/setupTests.ts"
-//     ],
-//     "moduleFileExtensions": [
-//       "ts",
-//       "js",
-//       "jsx",
-//       "tsx"
-//     ],
-//     "moduleNameMapper": {
-//       "#/(.*)": "<rootDir>/src/$1",
-//       "#/(.*)": "<rootDir>/$1"
-//     },
-//     "verbose": true,
-//     "collectCoverage": true,
-//     "coverageDirectory": "<rootDir>/coverage",
-//     "coverageReporters": [
-//       "json",
-//       "html"
-//     ],
-//     "coverageThreshold": {
-//       "*/**": {
-//         "branches": 0,
-//         "functions": 0,
-//         "lines": 0,
-//         "statements": -100
-//       }
-//     }
-//   },
+import type { Config } from 'jest';
+import { defaults } from 'jest-config';
+
+const config: Config = {
+    ...defaults,
+    testEnvironment: "jsdom",
+    verbose: true,
+    setupFilesAfterEnv: [
+        "<rootDir>/src/setupTests.tsx",
+    ],
+    moduleNameMapper: {
+        "#/(.*)": "<rootDir>/src/$1",
+    },
+    collectCoverage: true,
+    coverageDirectory: "<rootDir>/coverage",
+    coverageReporters: [
+        "json",
+        "html"
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0
+        },
+        "*/**": {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0
+        },
+    },
+    transform: {
+        "\\.[jt]sx?$": "ts-jest",
+        "^.+\\.css$": ["jest-transform-css", { modules: true }]
+    },
+    transformIgnorePatterns: [
+        "!node_modules/"
+    ],
+    globals: {
+        REACT_APP_NAME: "SMART_CODE",
+    },
+};
+
+export default config;
