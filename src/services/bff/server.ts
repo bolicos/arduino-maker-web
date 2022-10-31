284f7949236daa5db860a64d0397bf99f137b87a
analuciabolico <analuciabolico.dev@gmail.com>
2022-10-31 01:46:40 -0300

    feat: add new header axios

--- src/services/bff/server.ts
+++ src/services/bff/server.ts
@@ -7,4 +7,8 @@ export interface Object<T> extends Promise<Response<T>> { }
 export const bff = axios.create({
   baseURL: "https://arduino-maker-bff.herokuapp.com/",
   timeout: 30000,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "*/*",
+  }
 });
